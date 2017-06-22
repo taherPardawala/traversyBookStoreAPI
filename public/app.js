e9d05dad30a9ab54ad5da76f1d37e7ca8604a7cf
taherPardawala <skypunch2@gmail.com>
2017-06-22 06:07:43 +0530

    added initial code to controller books.js and make modifications to app.js

--- public/app.js
+++ public/app.js
@@ -1,27 +1,27 @@
-var myApp = angular.module('myApp', ['ngRouter']);
+var myApp = angular.module('myApp',['ngRoute']);
 
 myApp.config(function($routeProvider){
-    $routeProvider.when('/', {
-        controller: 'BooksController',
-        termplateUrl: 'views/books.html'
-    })
-    .when('/books', {
-        controller: 'BooksController',
-        termplateUrl: 'views/books.html'
-    })
-    .when('/books/details/:id', {
-        controller: 'BooksController',
-        termplateUrl: 'views/book_details.html'
-    })
-    .when('/books/add', {
-        controller: 'BooksController',
-        termplateUrl: 'views/add_book.html'
-    })
-    .when('/books/edit/:id', {
-        controller: 'BooksController',
-        termplateUrl: 'views/edit_book.html'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+	$routeProvider.when('/', {
+		controller:'BooksController',
+		templateUrl: 'views/books.html'
+	})
+	.when('/books', {
+		controller:'BooksController',
+		templateUrl: 'views/books.html'
+	})
+	.when('/books/details/:id',{
+		controller:'BooksController',
+		templateUrl: 'views/book_details.html'
+	})
+	.when('/books/add',{
+		controller:'BooksController',
+		templateUrl: 'views/add_book.html'
+	})
+	.when('/books/edit/:id',{
+		controller:'BooksController',
+		templateUrl: 'views/edit_book.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 });
