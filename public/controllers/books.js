a138e2c18bd157f955ec443fc9a84d3ae8b96797
taherPardawala <skypunch2@gmail.com>
2017-06-22 13:19:12 +0530

    Almost working completed most of it

--- public/controllers/books.js
+++ public/controllers/books.js
@@ -15,4 +15,24 @@ myApp.controller('BooksController', ['$scope', '$http', '$location', '$routePara
             console.log($scope.books)
         });
     }
+
+    $scope.addBook = function(){
+		console.log($scope.book);
+		$http.post('/api/books/', $scope.book).then(function(response){
+			window.location.href='/books';
+		});
+	}
+
+	$scope.updateBook = function(){
+		var id = $routeParams.id;
+		$http.put('/api/books/'+id, $scope.book).then(function(response){
+			window.location.href='/books';
+		});
+	}
+
+	$scope.removeBook = function(id){
+		$http.delete('/api/books/'+id).then(function(response){
+			window.location.href='/books';
+		});
+	}
 }]);
