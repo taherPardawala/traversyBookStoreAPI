8b13d30029a9e6c108024911d46f1a92029bd9ae
taherPardawala <skypunch2@gmail.com>
2017-06-22 06:28:24 +0530

    updated books.js and book.html with the basic getBooks and added a line of styling

--- public/controllers/books.js
+++ public/controllers/books.js
@@ -3,8 +3,8 @@ var myApp = angular.module('myApp');
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
     console.log('Books controller loaded');
     $scope.getBooks = function(){
-        $http.get('/api/books').success(function(response){
-            $scope.books = response;
+        $http.get('/api/books').then(function(response){
+            $scope.books = response.data;
         });
     }
 }]);
